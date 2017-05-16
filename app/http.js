ac3faac964dd9517f91485b0c6cba55e8ddc34c4
Gabriel <gabriel.l.maljkovich@gmail.com>
2017-05-16 17:07:54 -0300

    segundo intento

--- app/http.js
+++ app/http.js
@@ -3,12 +3,9 @@ const HTTP = axios.create({
   baseURL: 'https://desapp-backend.herokuapp.com',
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Origin': 'http://localhost:80'
+    'Access-Control-Allow-Origin': '*'
   }
 });
-HTTP.defaults.headers.post.Origin = 'http://localhost:80';
-HTTP.defaults.headers.common.Origin = 'http://localhost:80';
 HTTP.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 module.exports = HTTP;
